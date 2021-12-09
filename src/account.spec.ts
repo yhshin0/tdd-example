b4b6dc32bc2f9640892163a1252237aacb2f3148
yhshin0 <asyh1047@naver.com>
2021-12-09 21:35:30 +0900

    feat: 두 번째 질문(잔고 조회)
    - 잔고 조회 기능을 위한 테스트 케이스 작성

--- src/account.spec.ts
+++ src/account.spec.ts
@@ -1,7 +1,15 @@
+import { fail } from 'assert';
 import { Account } from './account';
 
 function testAccount() {
-  const account = new Account();
+  const account = new Account(10000);
+}
+
+function testGetBalance() {
+  const account = new Account(10000);
+  if (account.getBalance() !== 10000) {
+    fail();
+  }
 }
 
 describe('test', () => {
@@ -12,5 +20,9 @@ describe('test', () => {
       console.log(error.message);
     }
     console.log('성공');
+  });
+
+  it('에치 금액이 있는 계좌 생성에 성공한다', () => {
+    testGetBalance();
   });
 });
--- src/account.ts
+++ src/account.ts
@@ -1,1 +1,7 @@
-export class Account {}
+export class Account {
+  constructor(i: number) {}
+
+  getBalance() {
+    return 0;
+  }
+}
