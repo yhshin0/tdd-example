39ea2fa3682469d618cd3bbf1825b9f94166fad2
yhshin0 <asyh1047@naver.com>
2021-12-09 21:33:20 +0900

    feat: 첫 번째 정제
    - 리팩토링 적용
    - Jest 테스트 프레임워크 적용

--- src/account.spec.ts
+++ src/account.spec.ts
@@ -2,17 +2,15 @@ import { Account } from './account';
 
 function testAccount() {
   const account = new Account();
-  if (account === null) {
-    throw new Error('계좌 생성 실패');
-  }
 }
 
-(function main() {
-  try {
-    testAccount();
-  } catch (error) {
-    console.log('실패');
-    return;
-  }
-  console.log('성공');
-})();
+describe('test', () => {
+  it('계좌 생성에 성공한다', () => {
+    try {
+      testAccount();
+    } catch (error) {
+      console.log(error.message);
+    }
+    console.log('성공');
+  });
+});
